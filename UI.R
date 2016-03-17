6e01253667e2dea1249fa50124c231c21abd242b
Cliff <enkentaylor@yahoo.com>
2016-03-17 16:56:03 -0400

    Added Bryan's widgets to topic one and two.

--- UI.R
+++ UI.R
@@ -1,7 +1,10 @@
+library(shinysky)
 library(shiny)
+my_autocomplete_list <- c("Web Developer", "Software Developer", "Finance", "Biologist",
+                          "Chemist", "Physist", "Doctor", "Engineer", "Manager")
 
 # Define UI for application that draws a histogram
-shinyUI(navbarPage("Programmer Employment",
+shinyUI(navbarPage("Student Loan Data",
                    tabPanel("Introduction",
                             sidebarLayout(
                               sidebarPanel(position = "right",
@@ -13,23 +16,67 @@ shinyUI(navbarPage("Programmer Employment",
                             mainPanel(
                                 h1("Intent"),
                                 hr(),
-                                p("The  goal  of  this  app is to give basic  details about the employment situation
+                                p("**SAMPLE** The  goal  of  this  app is to give basic  details about the employment situation
                                   for computer  programmers  for the nation, by  state, and for each county in North
                                   Carolina.  Our  information  is  taken straight  from BLS.GOV, the Bureau of Labor
                                   and Statistics, and presented using built in R  functions to represent statistical
                                   analysis."),
                                 hr(),
                                 h1("Goals"),
-                                p("Our goals are to accurately represent the current job outlook for CS students who
+                                p("**SAMPLE** Our goals are to accurately represent the current job outlook for CS students who
                                   plan  to  identify  as  'Computer Programmers' who are entering the workforce upon
                                   graduation.  We decided to use R for it's data analysis features for this task. We
                                   also  chose  to  utilize  the  shiny  package  for R in order to develop a web app 
                                   because  it is  a good example of the simplicity that R offers to its users, while 
                                   providing powerful analytical support.")
                               )
-                            )),
-                   tabPanel("Topic 1"),
-                   tabPanel("Topic 2"),
+                            )
+                   ),
+                   tabPanel("Topic 1",
+                            sidebarLayout(
+                              sidebarPanel(
+                                selectInput(inputId = "state",
+                                            label = "Choose a State",
+                                            choices = state.name),
+                                            textInput.typeahead(id="search",
+                                                    placeholder="Choose a Job",
+                                                    local=data.frame(name=c(my_autocomplete_list)),
+                                                    valueKey = "name",
+                                                    tokens=c(1:length(my_autocomplete_list)),
+                                                    template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>"),
+                                                    
+                                )),
+                              
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                plotOutput("distPlot")
+                              )
+                            )
+                   ),
+                   tabPanel("Topic 2",
+                            sidebarLayout(
+                              sidebarPanel(
+                                selectInput(inputId = "state",
+                                            label = "Choose a State",
+                                            choices = state.name),
+                                
+                                textInput.typeahead(id="search",
+                                                    placeholder="Choose a Job",
+                                                    local=data.frame(name=c(my_autocomplete_list)),
+                                                    valueKey = "name",
+                                                    tokens=c(1:length(my_autocomplete_list)),
+                                                    template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>")
+                                                    
+                                )),
+                              
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                plotOutput("distPlot")
+                              )
+                            )
+                   ),
                    tabPanel("Topic 3"),
                    tabPanel("Topic 4"),
                    navbarMenu("More",
